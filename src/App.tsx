e8bbbed6ed5403bca214434f5e2b5cb9e8f10a94
Eduardo Tejada <et3858@gmail.com>
2024-02-08 16:47:55 -0300

    Query for dollars

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
+import { getRequest } from './api';
+import { DollarType } from './types';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [dollars, setDollars] = useState<DollarType[]>([]);
+
+    useEffect(() => {
+        const startRequest = async () => {
+            const params = { start_date: "2024-02-01", end_date: "2024-02-08" };
+
+            try {
+                const response = await getRequest("", params);
+                setDollars(response);
+            } catch (err) {
+                console.warn(err);
+            }
+        };
+
+        startRequest();
+    }, []);
+
+
+    useEffect(() => {
+        console.log(dollars)
+    }, [dollars]);
+
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                {dollars.map((e: DollarType) => (
+                    <tr key={e.date.toString()}>
+                        <td>{e.date.toString()}</td>
+                        <td>{e.value}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </>
     );
 }
