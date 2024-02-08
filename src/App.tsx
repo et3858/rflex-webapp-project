398afe4fac2763a6e0449f045db8d45640ea13a8
Eduardo Tejada <et3858@gmail.com>
2024-02-08 20:10:53 -0300

    Implement redux in webapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,87 @@
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { RootStateType } from './redux/reducers/rootReducer';
 import { getRequest } from './api';
 import { DollarType } from './types';
+import { ReduxActionType } from './enums/redux-enums';
 import './App.css';
 
 function App() {
-    const [dollars, setDollars] = useState<DollarType[]>([]);
+    const dollars: DollarType[] = useSelector((state: RootStateType) => state.list.dollars);
+    const dispatch = useDispatch();
+
+    const [startDate] = useState<string>("2024-02-01");
+    const [endDate] = useState<string>("2024-02-08");
+
 
     useEffect(() => {
         const startRequest = async () => {
-            const params = { start_date: "2024-02-01", end_date: "2024-02-08" };
+            const params = { start_date: startDate, end_date: endDate };
 
             try {
                 const response = await getRequest("", params);
-                setDollars(response);
+
+                dispatch({
+                    type: ReduxActionType.FILL_LIST,
+                    payload: response
+                });
             } catch (err) {
                 console.warn(err);
             }
         };
 
-        startRequest();
+        if (dollars.length === 0) {
+            startRequest();
+        }
     }, []);
 
 
-    useEffect(() => {
-        console.log(dollars)
-    }, [dollars]);
+    const handleClearList = () => {
+        dispatch({ type: ReduxActionType.CLEAR_LIST });
+    };
+
+
+    const handleUpdateElement = (e: DollarType) => {
+        dispatch({
+            type: ReduxActionType.UPDATE_ELEMENT,
+            dollar_id: e.id,
+            payload: { value: 999.99 }
+        })
+    };
+
+
+    const handleRevomeElement = (e: DollarType) => {
+        dispatch({
+            type: ReduxActionType.REMOVE_ELEMENT,
+            dollar_id: e.id,
+        })
+    };
 
 
     return (
         <>
+
+            <button onClick={handleClearList}>Clear list</button>
+
             <table>
                 <thead>
                     <tr>
                         <th>Date</th>
                         <th>Value</th>
+                        <th>Options</th>
                     </tr>
                 </thead>
 
                 <tbody>
                 {dollars.map((e: DollarType) => (
-                    <tr key={e.date.toString()}>
+                    <tr key={e.id}>
                         <td>{e.date.toString()}</td>
                         <td>{e.value}</td>
+                        <td>
+                            <button onClick={() => handleUpdateElement(e)}>Actualizar</button>
+                            <button onClick={() => handleRevomeElement(e)}>Borrar</button>
+                        </td>
                     </tr>
                 ))}
                 </tbody>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+/* Begin Redux things */
+import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from "./redux/store";
+/* End Redux things */
+
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Provider store={store}>
+        <PersistGate loading={<p>loading...</p>} persistor={persistor}>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </PersistGate>
+    </Provider>
 );
