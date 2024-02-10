c307882eddb6b932a65b0984cc25f7ef33b4c6f7
Eduardo Tejada <et3858@gmail.com>
2024-02-10 19:01:08 -0300

    Create datepickers block

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AgAreaSeriesOptions, AgLineSeriesOptions } from "ag-charts-community";
 import { ColDef, INumberCellEditorParams } from 'ag-grid-community';
@@ -7,10 +7,13 @@ import { RootStateType } from './redux/reducers/rootReducer';
 import { getRequest } from './api';
 import { DollarType } from './types';
 import { ReduxActionType } from './enums/redux-enums';
+import { formatDateString } from './utils';
 
 import LineChart from './components/ag_charts/LineChart';
 import AreaChart from './components/ag_charts/AreaChart';
 import Table from './components/ag-grid/Table';
+import DatePicker from './components/DatePicker';
+import Button from './components/Button';
 
 import './App.css';
 
@@ -56,9 +59,17 @@ function App() {
     const dollars: DollarType[] = useSelector((state: RootStateType) => state.list.dollars);
     const dispatch = useDispatch();
 
-    const [startDate] = useState<string>("2024-01-25");
-    const [endDate] = useState<string>("2024-02-08");
+    const [startDate, setStartDate] = useState<Date | null>(null);
+    const [endDate, setEndDate] = useState<Date | null>(null);
     const [selectedRows, setSelectedRows] = useState<number[]>([]);
+
+
+    const validDates = useMemo(() => {
+        return (
+            startDate instanceof Date && !isNaN(startDate.getTime()) &&
+            endDate instanceof Date && !isNaN(endDate.getTime())
+        );
+    }, [startDate, endDate]);
 
 
     useEffect(() => {
@@ -68,7 +79,10 @@ function App() {
 
 
     const startRequest = async () => {
-        const params = { start_date: startDate, end_date: endDate };
+        const params = {
+            start_date: formatDateString(startDate as Date),
+            end_date: formatDateString(endDate as Date),
+        };
 
         try {
             const response = await getRequest("", params);
@@ -118,8 +132,41 @@ function App() {
     };
 
 
+    const handleClick = () => {
+        startRequest();
+    };
+
+
     return (
         <>
+            <div style={{
+                display: 'flex',
+                alignItems: 'left',
+                gap: '4px',
+                marginBottom: '8px',
+            }}>
+                <DatePicker
+                    value={startDate}
+                    placeholder='Selecciona una fecha'
+                    onChange={((e: Date) => setStartDate(e))}
+                />
+
+                <DatePicker
+                    value={endDate}
+                    placeholder='Selecciona una fecha'
+                    onChange={((e: Date) => setEndDate(e))}
+                />
+
+                <Button
+                    color="blue"
+                    appearance="primary"
+                    onClick={handleClick}
+                    disabled={!validDates}
+                >
+                    Buscar
+                </Button>
+            </div>
+
             <LineChart
                 title={'My line chart of dollars'}
                 series={LINE_SERIES}
