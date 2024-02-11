ba821e28d772ca8dee6f26a0872ee3e6067c6d56
Eduardo Tejada <et3858@gmail.com>
2024-02-10 21:16:28 -0300

    Minor changes

--- src/components/ag-grid/Table.tsx
+++ src/components/ag-grid/Table.tsx
@@ -4,12 +4,12 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-quartz.css';
 
 // Row Data Interface
-interface IRow {
-    make: string;
-    model: string;
-    price: number;
-    electric: boolean;
-}
+// interface IRow {
+//     make: string;
+//     model: string;
+//     price: number;
+//     electric: boolean;
+// }
 
 interface IProps {
     colDefs: ColDef[],
@@ -40,13 +40,6 @@ function Table({
     };
 
     return (
-        /*
-        <div
-        className={"ag-theme-quartz"}
-        style={{ width: '100%', height: '100%' }}
-        className="ag-theme-quartz" style={{ height: 500 }}
-        >
-        */
         <div className="ag-theme-quartz" style={{ height: 500, width: 800 }}>
             <AgGridReact
                 rowData={rowData}
